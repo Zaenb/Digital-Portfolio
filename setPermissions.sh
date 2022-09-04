85c3608fc85024e622b5cd17b059f7450e5314c9
Zaenb Al Ameri <zaenb.alameri@student.allenisd.org>
2022-09-04 03:42:59 -0500

    hey

--- setPermissions.sh
+++ setPermissions.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Allow this and descenant directories to be traversed by everyone, ignoring hidden directories
+find -L "$PWD" -not -path "*/.*" -type d -exec chmod a+X {} \;
 
-chmod -R a+rX  ~/Digital\ Portfolio/
+# Allow everyone read access to all files in this hierarchy with an extention of
+# .html, .png, or .jpg
+find -L "$PWD" -not -path "*/.*" -type f \( -name "*.html" -o -name "*.png" -o -name "*.jpg" \) -exec chmod a+r {} \;
+
